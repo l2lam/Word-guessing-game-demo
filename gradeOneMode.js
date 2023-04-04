1ddb71464034d780eda546ddc23df1b8a23ddc39
KRcopy <elandyg1@gmail.com>
2023-04-04 10:58:45 -0400

    added if else, game is not starting

--- gradeOneMode.js
+++ gradeOneMode.js
@@ -1,10 +1,18 @@
-class gradeOneGame extend game(){
+class gradeOneGame extends game(){
   constructor(phrases, noGuessChar = '_', lives = 5, bgImage = null){
     super(phrases, noGuessChar, lives, bgImage)
   }
   
   gameStatSolved(){
   if (this.score > 300){
+    
+  }else if (this.score >= 3000){
+    
+  }else if (this.score >= 700){
+    
+  }else if (this.score > 1500){
+    
+  }else if (this.score > 500){
     
   }
   this.drawSolvedMessage()
